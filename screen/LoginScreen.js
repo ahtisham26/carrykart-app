14b4b1358687571f217d9be3efe4793ea2c0c2e8
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-04-14 19:48:01 +0530

    Update LoginScreen.js

--- screen/LoginScreen.js
+++ screen/LoginScreen.js
@@ -1,3 +1,4 @@
+export default function LoginScreen({ setUser }) {
 import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { auth } from "../firebase/config";
