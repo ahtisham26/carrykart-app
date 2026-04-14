11f79788d60349e696c8c957f16f8eab8fbe83fa
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-04-15 00:03:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import * as Notifications from "expo-notifications";
 
 export default function App() {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    Notifications.requestPermissionsAsync();
+  }, []);
 
   if (!user) {
     return <LoginScreen setUser={setUser} />;
   }
 
-  return (
-    <>
-      {/* HOME SCREEN PLACEHOLDER */}
-      <Text>Welcome to CarryKart 🚀</Text>
-    </>
-  );
+  return <HomeScreen />;
 }
