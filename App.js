30890b267203d44656eef3b9edb122e7c3c656cc
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-04-15 13:50:24 +0530

    Remove unused useEffect import from App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import LoginScreen from "./screen/LoginScreen";
 import HomeScreen from "./screen/HomeScreen";
 
