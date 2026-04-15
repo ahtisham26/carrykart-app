0151d76bb6f01b953701329a5232d124518c183a
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-04-15 13:50:56 +0530

    Update LoginScreen.js

--- screen/LoginScreen.js
+++ screen/LoginScreen.js
@@ -1,70 +1,46 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { auth } from "../firebase/config";
-import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword
-} from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function LoginScreen({ setUser }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const signup = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      Alert.alert("Signup Success");
-      setUser(true);
-    } catch (e) {
-      Alert.alert(e.message);
-    }
-  };
-
   const login = async () => {
     try {
-      await signInWithEmailAndPassword(auth, email, password);
-      Alert.alert("Login Success");
-      setUser(true);
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      setUser(res.user);
+      alert("Login Success");
     } catch (e) {
-      Alert.alert(e.message);
+      alert(e.message);
     }
   };
 
   return (
     <View style={{ flex: 1, justifyContent: "center", padding: 20 }}>
-      <Text style={{ fontSize: 26, marginBottom: 20 }}>
-        CarryKart Login
-      </Text>
+      <Text style={{ fontSize: 26, marginBottom: 20 }}>CarryKart Login</Text>
 
       <TextInput
         placeholder="Email"
         onChangeText={setEmail}
-        style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}
         autoCapitalize="none"
+        style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}
       />
 
       <TextInput
         placeholder="Password"
         secureTextEntry
         onChangeText={setPassword}
-        style={{ borderWidth: 1, padding: 10 }}
+        style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}
       />
 
       <TouchableOpacity
         onPress={login}
-        style={{ backgroundColor: "black", padding: 15, marginTop: 15 }}
+        style={{ backgroundColor: "black", padding: 15 }}
       >
         <Text style={{ color: "white", textAlign: "center" }}>
           Login
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={signup}
-        style={{ backgroundColor: "green", padding: 15, marginTop: 10 }}
-      >
-        <Text style={{ color: "white", textAlign: "center" }}>
-          Signup
         </Text>
       </TouchableOpacity>
     </View>
