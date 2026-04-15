0282e41bd00afee3ce63b059cbee42a840bcd75e
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-04-15 08:20:31 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import LoginScreen from "./screen/LoginScreen";
 import HomeScreen from "./screen/HomeScreen";
-import * as Notifications from "expo-notifications";
 
 export default function App() {
   const [user, setUser] = useState(null);
