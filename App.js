2d8ebb20391859c587568191784f0be5a21e54af
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-04-15 16:36:05 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,37 +1,14 @@
-import React, { useState, useEffect } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useState } from "react";
 import LoginScreen from "./screen/LoginScreen";
 import HomeScreen from "./screen/HomeScreen";
+import AdminScreen from "./screen/AdminScreen";
 
 export default function App() {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    loadUser();
-  }, []);
+  if (!user) return <LoginScreen setUser={setUser} />;
 
-  const loadUser = async () => {
-    const data = await AsyncStorage.getItem("user");
-    if (data) setUser(JSON.parse(data));
-    setLoading(false);
-  };
+  if (user.role === "admin") return <AdminScreen user={user} />;
 
-  const login = async (userData) => {
-    await AsyncStorage.setItem("user", JSON.stringify(userData));
-    setUser(userData);
-  };
-
-  const logout = async () => {
-    await AsyncStorage.removeItem("user");
-    setUser(null);
-  };
-
-  if (loading) return null;
-
-  if (!user) {
-    return <LoginScreen setUser={login} />;
-  }
-
-  return <HomeScreen user={user} logout={logout} />;
+  return <HomeScreen user={user} />;
 }
