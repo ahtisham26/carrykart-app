44894ef3c81f331ab8665bd23b4c74058b99e5b2
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-04-15 14:41:05 +0530

    Update storageBucket URL in firebase config

--- firebase/config.js
+++ firebase/config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDsUlNim4HMGIIT53qKIbK41w7CjrK_PY",
   authDomain: "carrykart-6837b.firebaseapp.com",
   projectId: "carrykart-6837b",
-  storageBucket: "carrykart-6837b.firebasestorage.app",
+  storageBucket: "carrykart-6837b.appspot.com",
   messagingSenderId: "410610080619",
   appId: "1:410610080619:web:5e50ebca9d7f21f9b68a2e"
 };
