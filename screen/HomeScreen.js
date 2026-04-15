70ab2334bac1a6347fd5c61829bd65d9a761ba08
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-04-15 16:38:11 +0530

    Update HomeScreen.js

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,26 +1,10 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text } from "react-native";
 
-export default function HomeScreen({ user, logout }) {
+export default function HomeScreen({ user }) {
   return (
-    <View style={{ flex: 1, justifyContent: "center", padding: 20 }}>
-
-      <Text>Welcome: {user.email}</Text>
-      <Text>Role: {user.role}</Text>
-
-      {user.role === "admin" ? (
-        <Text style={{ marginTop: 20 }}>ADMIN DASHBOARD ACCESS</Text>
-      ) : (
-        <Text style={{ marginTop: 20 }}>USER DASHBOARD</Text>
-      )}
-
-      <TouchableOpacity
-        onPress={logout}
-        style={{ backgroundColor: "red", padding: 15, marginTop: 20 }}
-      >
-        <Text style={{ color: "white" }}>Logout</Text>
-      </TouchableOpacity>
-
+    <View>
+      <Text>Welcome {user.name}</Text>
     </View>
   );
 }
