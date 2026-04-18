20a8c1431fe581c632548158e8e532ea5cd9bdab
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-04-19 00:00:41 +0530

    Modify HomeScreen to show user email and add order form
    Updated welcome message to display user's email and added CreateOrderScreen component.

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { View, Text } from "react-native";
+import CreateOrderScreen from "./CreateOrderScreen";
 
 export default function HomeScreen({ user }) {
   return (
-    <View>
-      <Text>Welcome {user.name}</Text>
+    <View style={{ flex: 1 }}>
+      <Text style={{ fontSize: 20, margin: 20 }}>
+        Welcome {user.email}
+      </Text>
+
+      {/* ORDER FORM */}
+      <CreateOrderScreen user={user} />
     </View>
   );
 }
