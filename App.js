04dec8f5c13de5c6ec52a6697fdcaa1f7e68d9c3
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-04-19 14:33:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import LoginScreen from "./screen/LoginScreen";
 import HomeScreen from "./screen/HomeScreen";
 import AdminScreen from "./screen/AdminScreen";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { View, ActivityIndicator } from "react-native";
 
 export default function App() {
   const [user, setUser] = useState(null);
@@ -11,49 +11,33 @@ export default function App() {
 
   // AUTO LOGIN
   useEffect(() => {
-    checkUser();
-  }, []);
-
-  const checkUser = async () => {
-    try {
+    const loadUser = async () => {
       const savedUser = await AsyncStorage.getItem("user");
       if (savedUser) {
         setUser(JSON.parse(savedUser));
       }
-    } catch (e) {
-      console.log(e);
-    } finally {
       setLoading(false);
-    }
+    };
+
+    loadUser();
+  }, []);
+
+  const loginUser = async (data) => {
+    setUser(data);
+    await AsyncStorage.setItem("user", JSON.stringify(data));
   };
 
-  // LOGIN HANDLER
-  const handleLogin = async (userData) => {
-    setUser(userData);
-    await AsyncStorage.setItem("user", JSON.stringify(userData));
-  };
-
-  // LOGOUT
-  const logout = async () => {
+  const logoutUser = async () => {
     setUser(null);
     await AsyncStorage.removeItem("user");
   };
 
-  if (loading) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" color="#800020" />
-      </View>
-    );
-  }
+  if (loading) return null;
 
-  if (!user) {
-    return <LoginScreen setUser={handleLogin} />;
-  }
+  if (!user) return <LoginScreen setUser={loginUser} />;
 
-  if (user.role === "admin") {
-    return <AdminScreen user={user} logout={logout} />;
-  }
+  if (user.role === "admin")
+    return <AdminScreen user={user} logout={logoutUser} />;
 
-  return <HomeScreen user={user} logout={logout} />;
+  return <HomeScreen user={user} logout={logoutUser} />;
 }
