4c7939b0bf051a25a9bd3102aeb54115e1fd6a73
ahtisham26 <ahtishamulhaq086@gmail.com>
2026-05-02 16:44:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeScreen from "./screens/HomeScreen";
 import OrdersScreen from "./screens/OrdersScreen";
 import CreateOrder from "./screens/CreateOrder";
-import AdminDashboard from "./screens/AdminDashboard";
 
 const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator();
-
-function Tabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Orders" component={OrdersScreen} />
-      <Tab.Screen name="Create" component={CreateOrder} />
-    </Tab.Navigator>
-  );
-}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Main" component={Tabs} options={{ headerShown: false }} />
-        <Stack.Screen name="Admin" component={AdminDashboard} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarActiveTintColor: "#4CAF50",
+          tabBarInactiveTintColor: "gray",
+
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Cart") {
+              iconName = focused ? "cart" : "cart-outline";
+            } else if (route.name === "Add") {
+              iconName = focused ? "add-circle" : "add-circle-outline";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Cart" component={OrdersScreen} />
+        <Tab.Screen name="Add" component={CreateOrder} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
